b0aa1906bf962733a5288df31d7cd27087b75706
Ben Foxall <ben@pusher.com>
2016-10-26 13:47:14 +0100

    Add pusher auth backend

---
+++ backend/config.example.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pusher: {
+    appId: 'CONFIG',
+    key: 'CONFIG',
+    secret: 'CONFIG',
+    cluster: 'CONFIG',
+    encrypted: true
+  }
+}
--- backend/handler.js
+++ backend/handler.js
@@ -6,3 +6,21 @@ module.exports.hello = (event, context, cb) => {
 };
 
 // You can add more handlers here, and reference them in serverless.yml
+
+var config = require('./config')
+
+var Pusher = require('pusher')
+
+var pusher = new Pusher(config.pusher)
+
+module.exports.auth = (event, context, cb) => {
+
+  const socket_id  = event.body.socket_id
+  const channel_name = event.body.channel_name
+
+  if(!(socket_id && channel_name))
+    return cb(new Error('[500] socket and channel not provided'))
+
+  cb(null, pusher.authenticate(socket_id, channel_name))
+
+}
